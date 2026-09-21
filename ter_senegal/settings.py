c06272aadc95d1155d8b48304478f9c21f67aece
CISSEDAOUDA <dcussu2314@gmail.com>
2026-09-21 10:14:38 +0000

    reglage du setting.py

--- ter_senegal/settings.py
+++ ter_senegal/settings.py
@@ -10,10 +10,17 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = os.environ.get('SECRET_KEY', 'django-insecure-change-me-before-production')
 
-DEBUG = os.environ.get('DEBUG', 'True') == 'True'
+import os
 
-ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', 'localhost,127.0.0.1').split(',')
+import os
 
+DEBUG = os.environ.get("DEBUG", "True") == "True"
+
+ALLOWED_HOSTS_ENV = os.environ.get("ALLOWED_HOSTS")
+if ALLOWED_HOSTS_ENV:
+    ALLOWED_HOSTS = ALLOWED_HOSTS_ENV.split(",")
+else:
+    ALLOWED_HOSTS = ["localhost", "127.0.0.1"] if DEBUG else []
 
 INSTALLED_APPS = [
     'django.contrib.admin',
